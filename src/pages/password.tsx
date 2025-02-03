e46f96b46d99e41c3e6342b7cd6621322c1a45c8
flod <jonas.meuleman@icloud.com>
2025-02-04 00:50:36 +0100

    Update password.tsx

--- src/pages/password.tsx
+++ src/pages/password.tsx
@@ -1,6 +1,8 @@
 // pages/password.tsx
 import React from 'react';
 import Head from 'next/head';
+import Header from '../components/layout/Header'
+import Footer from '../components/layout/Footer'
 import PasswordForm from '../components/auth/PasswordForm';
 import styles from '../styles/password.module.css';
 
@@ -11,9 +13,11 @@ const PasswordPage: React.FC = () => {
         <title>Enter Password - TNT Tag History</title>
         <meta name="description" content="Enter the password to access the TNT Tag History site" />
       </Head>
+      <Header />
       <main className={styles.mainContent}>
         <PasswordForm />
       </main>
+      <Footer />
     </>
   );
 };
